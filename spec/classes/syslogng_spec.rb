858579c8e1b1d293455a634ab1b3951fb53bd623
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-12 12:55:12 +0200

    check for validate_re failure on invalide ensure

--- spec/classes/syslogng_spec.rb
+++ spec/classes/syslogng_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe 'syslogng' do
+  subject do
+    # This makes sure the function is loaded within each test
+    function_name = Puppet::Parser::Functions.function(:validate_re)
+    scope.method(function_name)
+  end
   context "callable with params" do
     let(:params) do
       {
-        :ensure => 'undef',
+        :ensure => 'present',
       }
     end
   end
@@ -16,6 +21,16 @@ describe 'syslogng' do
     end
     it {
       should contain_package('syslog-ng').with({:ensure => 'present'})
+    }
+  end
+  context "will fail on invalid ensure parameter" do
+    let (:params) do
+      {
+        :ensure => 'invalid'
+      }
+    end
+    it {
+      expect { subject.call ['invalid'] }.to raise_error Puppet::Error
     }
   end
 end
