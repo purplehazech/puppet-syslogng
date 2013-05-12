0b6c471035023e9c4b38f1c12396f11851d2e139
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-12 12:55:27 +0200

    validate ensure param

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,8 @@
 # The syslog class is used to install install syslog-ng.
 #
 class syslogng ($ensure = present) {
+  validate_re($ensure, [ '^present', '^absent' ])
+
   package { 'syslog-ng':
     ensure => $ensure
   }
