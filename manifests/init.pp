86dadc7b517e93f6283541a7f4662d87cab5cd49
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-12 19:35:38 +0200

    implement service and dirs

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,10 +10,21 @@
 class syslogng ($ensure = present) {
   validate_re($ensure, [ '^present', '^absent' ])
 
-notify{"$ensure":}
   $ensure_file = $ensure ? {
     present => file,
     default => $ensure,
+  }
+  $ensure_directory = $ensure ? {
+    present => directory,
+    default => $ensure,
+  }
+  $ensure_service = $ensure ? {
+    present => 'running',
+    default => 'stopped',
+  }
+  $enable_service = $ensure ? {
+    present => true,
+    default => false,
   }
 
   package { 'syslog-ng':
@@ -22,13 +33,27 @@ notify{"$ensure":}
     [
       '/etc/syslog-ng/syslog-ng.conf',
       '/etc/syslog-ng/scl.conf',
-      '/etc/syslog-ng/modules.conf'
+      '/etc/syslog-ng/modules.conf',
     ]:
       ensure => $ensure_file,
       source => [
         'puppet:///modules/syslogng/scl/syslog-ng.conf',
         'puppet:///modules/syslogng/scl/scl.conf',
-        'puppet:///modules/syslogng/scl/modules.conf'
-      ]
+        'puppet:///modules/syslogng/scl/modules.conf',
+      ];
+    [
+      '/etc/syslog-ng/patterndb.d',
+      '/etc/syslog-ng/syslog-ng.conf.d',
+      '/etc/syslog-ng/syslog-ng.conf.d/destination.d',
+      '/etc/syslog-ng/syslog-ng.conf.d/filter.d',
+      '/etc/syslog-ng/syslog-ng.conf.d/source.d',
+      '/etc/syslog-ng/syslog-ng.conf.d/log.d',
+      '/etc/syslog-ng/syslog-ng.conf.d/option.d',
+      '/etc/syslog-ng/syslog-ng.conf.d/service.d',
+    ]:
+      ensure => $ensure_directory;
+  } ~> service { 'syslog-ng':
+    ensure => $ensure_service,
+    enable => $enable_service,
   }
 }
