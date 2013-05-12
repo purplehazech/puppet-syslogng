dfaf204e0845a11ce51a367cead1dc5f1ddc96db
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-12 21:10:22 +0200

    spec for destination defines

---
+++ spec/defines/syslogng__destination_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'syslogng::destination' do
+  context "callable with params" do
+    let(:title) { 'messages' }
+    let(:params) do
+      {
+        :ensure => 'present',
+      }
+    end
+    it {
+      should contain_file("/etc/syslog-ng/syslog-ng.conf.d/destination.d/messages.conf").with(
+        {
+	  :ensure => 'file',
+	  :source => 'puppet:///modules/syslogng/scl/syslog-ng.conf.d/destination.d/message.conf'
+	}
+      )
+    }
+  end
+  context "removeable" do
+    let(:title) { 'messages' }
+    let(:params) do
+      {
+	:ensure => 'absent',
+      }
+    end
+    it {
+      should contain_file("etc/syslog-ng/syslog-ng.conf.d/destination.d/messages.conf").with({:ensure => 'absent'})
+    }
+  end
+end
