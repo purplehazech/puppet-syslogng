8114b3a2c2e6e4f52eccbc248665ffbdce16069b
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-12 20:16:57 +0200

    path validation and more rubustness

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,9 +6,12 @@
 # [*ensure*]
 #  main module switch used to enable or disable installation and configuration
 #  of syslog-ng package.
+# [*conf_dir*]
+#  base directory of syslog-ng config files.
 #
-class syslogng ($ensure = present, $conf_dir = '/etc/syslog-ng/') {
+class syslogng ($ensure = present, $conf_dir = '/etc/syslog-ng') {
   validate_re($ensure, [ '^present', '^absent' ])
+  validate_absolute_path($conf_dir)
 
   $ensure_file = $ensure ? {
     present => file,
@@ -30,12 +33,12 @@ class syslogng ($ensure = present, $conf_dir = '/etc/syslog-ng/') {
   package { 'syslog-ng':
     ensure => $ensure
   } -> file {
-    "${conf_dir}syslog-ng.conf":
+    "${conf_dir}/syslog-ng.conf":
       ensure  => $ensure_file,
       content => template('syslogng/syslog-ng.conf.erb');
     [
-      "${conf_dir}scl.conf",
-      "${conf_dir}modules.conf",
+      "${conf_dir}/scl.conf",
+      "${conf_dir}/modules.conf",
     ]:
       ensure => $ensure_file,
       source => [
@@ -43,14 +46,14 @@ class syslogng ($ensure = present, $conf_dir = '/etc/syslog-ng/') {
         'puppet:///modules/syslogng/scl/modules.conf',
       ];
     [
-      "${conf_dir}patterndb.d",
-      "${conf_dir}syslog-ng.conf.d",
-      "${conf_dir}syslog-ng.conf.d/destination.d",
-      "${conf_dir}syslog-ng.conf.d/filter.d",
-      "${conf_dir}syslog-ng.conf.d/source.d",
-      "${conf_dir}syslog-ng.conf.d/log.d",
-      "${conf_dir}syslog-ng.conf.d/option.d",
-      "${conf_dir}syslog-ng.conf.d/service.d",
+      "${conf_dir}/patterndb.d",
+      "${conf_dir}/syslog-ng.conf.d",
+      "${conf_dir}/syslog-ng.conf.d/destination.d",
+      "${conf_dir}/syslog-ng.conf.d/filter.d",
+      "${conf_dir}/syslog-ng.conf.d/source.d",
+      "${conf_dir}/syslog-ng.conf.d/log.d",
+      "${conf_dir}/syslog-ng.conf.d/option.d",
+      "${conf_dir}/syslog-ng.conf.d/service.d",
     ]:
       ensure => $ensure_directory;
   } ~> service { 'syslog-ng':
