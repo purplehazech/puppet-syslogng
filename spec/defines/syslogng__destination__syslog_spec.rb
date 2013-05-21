75e6bef328eb3d07b3cb345a29a4ec33f3264982
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-21 23:07:46 +0200

    check for most params
    tls and timezone stuff is still missing though, those warrant their
    own checks

--- spec/defines/syslogng__destination__syslog_spec.rb
+++ spec/defines/syslogng__destination__syslog_spec.rb
@@ -98,4 +98,36 @@ describe 'syslogng::destination::syslog' do
       )
     }
   end
+  context "a heap of additional optional params" do
+    let(:title) { 'remote-server-hostname' }
+    let(:params) do
+      {
+        :port            => 123,
+	:no_multi_line   => true,
+	:flush_lines     => 10,
+	:flush_timeout   => 20,
+	:frac_digits     => 5,
+	:ip_tos          => 11,
+	:ip_ttl          => 1,
+	:keep_alive      => false,
+	:localip         => "1.1.1.1",
+	:locapport       => 1234,
+	:log_fifo_size   => 128,
+	:so_broadcast    => true,
+	:so_keepalive    => true,
+	:so_rcvbuf       => 256,
+	:so_sndbuf       => 1024,
+	:spoof_source    => true,
+	:suppress        => 12,
+	:template        => "my-template",
+	:template_escape => true,
+	:throttle        => 13,
+      }
+    end
+    it {
+      should contain_file("/etc/syslog-ng/syslog-ng.conf.d/destination.d/syslog_remote-server-hostname.conf").with_content(
+        /.*port\(123\).*flags\(no-multi-line\).*flush_line\(10\).*flush_timeout\(20\).*frac_digits\(5\).*ip_tos\(11\).*ip_ttl\(1\).*keep-alive\(no\).*localip\("1.1.1.1"\).*localport\(1234\).*log_fifo_size\(128\).*so_braodcast\(yes\).*so_keepalive\(yes\).*so_rcvbuf\(256\).*so_sndbuf\(1024\).*spoof_source\(yes\).*suppress\(12\).*template\("my-template"\).*template_escape\(yes\).*throttle\(13\).*/m
+      )
+    }
+  end
 end
