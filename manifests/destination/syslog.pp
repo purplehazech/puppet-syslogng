cb2797fcc9aaa8822284dadcbc19471516f0b509
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-21 23:41:18 +0200

    implement syslog destination params

--- manifests/destination/syslog.pp
+++ manifests/destination/syslog.pp
@@ -2,14 +2,33 @@
 #
 #
 define syslogng::destination::syslog (
-  $ensure    = present,
-  $conf_dir  = '/etc/syslog-ng',
-  $services  = {
+  $ensure          = present,
+  $conf_dir        = '/etc/syslog-ng',
+  $services        = {
     'syslog-ng' => {},
   },
-  $priority  = 00,
-  $transport = 'tcp',
-  $port      = undef
+  $priority        = 00,
+  $transport       = 'tcp',
+  $port            = undef,
+  $no_multi_line   = undef,
+  $flush_lines     = undef,
+  $flush_timeout   = undef,
+  $frac_digits     = undef,
+  $ip_tos          = undef,
+  $ip_ttl          = undef,
+  $keep_alive      = undef,
+  $localip         = undef,
+  $localport       = undef,
+  $log_fifo_size   = undef,
+  $so_broadcast    = undef,
+  $so_keepalive    = undef,
+  $so_rcvbuf       = undef,
+  $so_sndbuf       = undef,
+  $spoof_source    = undef,
+  $suppress        = undef,
+  $template        = undef,
+  $template_escape = undef,
+  $throttle        = undef
 ) {
   validate_re($ensure, ['^present$', '^absent$'])
   validate_absolute_path($conf_dir)
