def11efac1fcd0c898ff6b93a5935cd35c324ec3
Lucas Bickel <hairmare@purplehaze.ch>
2013-05-21 21:59:33 +0200

    implement transport setting

--- manifests/destination/syslog.pp
+++ manifests/destination/syslog.pp
@@ -2,16 +2,17 @@
 #
 #
 define syslogng::destination::syslog (
-  $ensure   = present,
-  $conf_dir = '/etc/syslog-ng',
-  $services = {
+  $ensure    = present,
+  $conf_dir  = '/etc/syslog-ng',
+  $services  = {
     'syslog-ng' => {},
   },
-  $priority = 00
+  $priority  = 00,
+  $transport = 'tcp'
 ) {
   validate_re($ensure, ['^present$', '^absent$'])
   validate_absolute_path($conf_dir)
-  validate_string($host)
+  validate_re($transport, ['^tcp$', '^udp$', '^tls$'])
 
   $ensure_file = $ensure ? {
     present => file,
