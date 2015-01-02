c5788a85ce3c7808ff4fa7ec95670dedd9212785
Lucas Bickel <hairmare@purplehaze.ch>
2015-01-02 15:53:50 +1000

    add coverage info

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-
 RSpec.configure do |c|
   c.filter_run_excluding :broken => true
 end
+
+at_exit { RSpec::Puppet::Coverage.report! }
